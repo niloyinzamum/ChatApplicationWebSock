d03430ca598a6f2630c3c38b736ef2216c56591e
niloy <niloy.inzamum@gmail.com>
2021-11-12 22:28:09 +0600

    in progress

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path= require('path')
 const http = require('http')
 const express = require('express');
 const socketio = require('socket.io');
+const { disconnect } = require('process');
 
 const app = express();
 const server = http.createServer(app)
@@ -12,11 +13,22 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //Run when client connects
 io.on('connection', socket =>{
-    console.log("New WS Connection...");
-
+    //message for new user
     socket.emit('message', 'Welcome to ChatCord')
+    //message for everyone except new user
+    socket.broadcast.emit('message', "New user joined!")
+    //message for everyone
+    socket.on('disconnect', ()=>{
+        io.emit('message','A user has left')
+    })
+    //Listen for messages from the client
+    socket.on('chatMessage', msg=>{
+        io.emit('message', msg)
+    })
+
 })
+
 
 const PORT = 3000|| process.env.PORT;
 
-app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
+server.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
